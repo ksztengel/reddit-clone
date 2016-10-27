f2c2af75cd931559230b1b2508c4c57f5e07a11f
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-27 10:33:39 -0600

    existing comments hide and show

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var app = angular.module('redditApp', [])
 
 app.controller('mainController', ['$scope', function($scope) {
-
+    $scope.sort='-votes'
     $scope.showCommentForm=false
     $scope.view={}
     $scope.view.postArray = [{
@@ -21,8 +21,8 @@ app.controller('mainController', ['$scope', function($scope) {
     }, {
         title: 'High Goal Polo',
         image: 'http://ryanpemblepolo.com/wp-content/uploads/2016/01/2015FinalHurlingham.jpg',
-        author: "",
-        description: "",
+        author: "Guards Polo Staff",
+        description: "There are 4 to 6 'chukkas' in a match and each one lasts for 7 minutes of actual play. At present, though due to change, when the ball goes out of play, or the whistle is blown, the clock is stopped and only restarted when the umpire calls 'Play'. At the end of the period a bell goes to signify that the 7 minutes are up, however there is a further 30 seconds of play unless the whistle is blown, the ball hits the boards, or the ball goes out of play. This extra 30 seconds does not apply to the last chukka unless the scores are equal. Should a match be drawn a 'sudden death' (i.e. first to score) chukka maybe played, and if still a draw at the end of that chukka, another one is played with the goalposts widened. There is an interval of 3 minutes between chukkas but 5 minutes at half time when traditionally spectators are invited to tread in the divots.",
         date: moment().calendar(),
         numberOfComments: 0,
         commentsVisible:false,
@@ -30,29 +30,19 @@ app.controller('mainController', ['$scope', function($scope) {
         votes: 0,
         id: 1
 
-    }, {
-        title: 'Family Time',
-        image: 'https://pbs.twimg.com/profile_images/1563380209/image_400x400.jpg',
-        author: "",
-        description: "",
-        date: moment().calendar(),
-        numberOfComments: 0,
-        commentsVisible:false,
-        comments: [],
-        votes: 0,
-        id: 2
-    }, {
+    },
+  {
         title: 'The Athletes',
         image: 'http://www.lospingosdeltaita.com/images/padrillos/1425323201_big.jpg',
-        author: "",
-        description: "",
+        author: "Polo Museum Staff",
+        description: "Polo mounts are called “ponies,” although the term “pony” is traditional and the mount is actually a full-sized horse. They range from 14.2 to 16 hands (58 to 64 inches) high at the withers, and weigh 900–1,100 pounds. The polo pony is selected for quick bursts of speed, stamina, agility and manoeuvrability. Temperament is critical: The horse must remain responsive under pressure and not become excited or difficult to control. They are trained to be handled with one hand on the reins, and to respond to the rider's leg and weight cues. A well-trained polo pony accounts for a great amount of the player's net worth to his team.",
         date: moment().calendar(),
         numberOfComments: 0,
         commentsVisible:false,
         comments: [{comment: "Beautiful horse!",
           author: "me"}],
         votes: 3,
-        id: 3
+        id: 2
     }]
 
 
